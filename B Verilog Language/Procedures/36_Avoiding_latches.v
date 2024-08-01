9f09265588bf2f614b2a70a864ae2b5ad7c605e2
trungthanh23 <trungnguyenthanh.2304@gmail.com>
2024-08-01 22:10:56 +0700

    Thêm file 22:10 01/08/2024

---
+++ B Verilog Language/Procedures/36_Avoiding_latches.v
@@ -0,0 +1,17 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [15:0] scancode,
+    output reg left,
+    output reg down,
+    output reg right,
+    output reg up  ); 
+    always @(*) begin
+        {left, down, right, up} = 4'b0;
+        case(scancode)
+            16'he06b: left = 1;
+            16'he072: down = 1;
+            16'he074: right = 1;
+            16'he075: up = 1;
+        endcase
+    end
+endmodule
